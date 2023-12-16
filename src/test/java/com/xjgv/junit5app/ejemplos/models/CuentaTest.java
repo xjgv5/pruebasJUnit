8035615b25392c4e6a603ce7b6e316a2e0e385bd
Javier Gomez <javier.gov@outlook.es>
2023-12-15 23:13:02 -0600

    Prueba de saldo

--- src/test/java/com/xjgv/junit5app/ejemplos/models/CuentaTest.java
+++ src/test/java/com/xjgv/junit5app/ejemplos/models/CuentaTest.java
@@ -11,10 +11,19 @@ class CuentaTest {
 
     @Test
     void testNombreCuenta() {
-        Cuenta cuenta = new Cuenta("Andres", new BigDecimal("12313131.123123"));
+        Cuenta cuenta = new Cuenta("Andres", new BigDecimal("1000.123"));
         String esperado = "Andres";
         String actual = cuenta.getPersona();
         assertEquals(esperado, actual);
         assertTrue(actual.equals("Andres"));
+    }
+
+    @Test
+    void testSaldoCuenta(){
+        Cuenta cuenta = new Cuenta("Andres", new BigDecimal("1000.123"));
+        assertEquals(1000.123, cuenta.getSaldo().doubleValue());
+        assertFalse(cuenta.getSaldo().compareTo(BigDecimal.ZERO) < 0);
+        assertTrue(cuenta.getSaldo().compareTo(BigDecimal.ZERO) > 0);
+
     }
 }
